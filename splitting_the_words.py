50cd5515f291a80217ff1f5997d281dd1682071a
Alina <126732144+SmartAlina@users.noreply.github.com>
2023-03-08 08:33:16 -0800

    splitting_the_words.py
    Fill in the blank to complete the “string_words” function. This function should split up the words in the given “string” and return the number of words in the “string”.  Complete the string operation and method needed in this function so that a function call like "string_words("Hello, World")" will return the output "2".

---
+++ splitting_the_words.py
@@ -0,0 +1,7 @@
+def string_words(string):
+    return len(string.split())
+
+print(string_words("Hello, World")) # Should print 2
+print(string_words("Python is awesome")) # Should print 3
+print(string_words("Keep going")) # Should print 2
+print(string_words("Have a nice day")) # Should print 4
