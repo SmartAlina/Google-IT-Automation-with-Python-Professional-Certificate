d8185922c5ddfcb18993ed52e5979d537fbb5029
Alina <126732144+SmartAlina@users.noreply.github.com>
2023-03-07 23:56:43 -0800

    modifying_students_grades.py
    Modify the student_grade function using the format method, so that it returns the phrase "X received Y% on the exam". For example, student_grade("Reed", 80) should return "Reed received 80% on the exam".

---
+++ modifying_students_grades.py
@@ -0,0 +1,10 @@
+def student_grade(name, grade):
+	return "{} received {}% on the exam".format(name, grade)
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
+# Should print:
+# Reed received 80% on the exam
+# Paige received 92% on the exam
+# Jesse received 85% on the exam
